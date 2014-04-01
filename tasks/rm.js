0b3cc6973e2b489661e66014d78af6821da612c1
adamrenklint <adam@renklint.com>
2014-04-01 14:38:40 +0200

    first commit, ported task from the main wunderlist repo

---
+++ tasks/rm.js
@@ -0,0 +1,32 @@
+var npath = require('path');
+var fs = require('fs');
+var child = require('child_process');
+
+module.exports = function (grunt) {
+
+  'use strict';
+
+  var glob = grunt.file.glob;
+  var async = grunt.util.async;
+
+  function FileRemover() {
+
+    var done = this.async();
+    var options = this.options();
+    var dir = npath.resolve(options.dir);
+    var patterns = options.patterns;
+
+    async.forEachLimit(patterns, 10, function (pattern, callback) {
+
+      var files = glob.sync(pattern, {
+        'cwd': dir
+      });
+      async.forEachLimit(files, 10, function (file, _callback) {
+        file = npath.resolve(dir, file);
+        child.exec('rm -rf ' + file, _callback);
+      }, callback);
+    }, done);
+  }
+
+  grunt.registerTask('secureFiles', FileRemover);
+};
